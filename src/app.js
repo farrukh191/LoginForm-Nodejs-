b7a88b859dace97b7a85d66b3fc98cab6f844de7
farrukhferoz <farrukhferoz12@gmail.com>
2021-08-02 23:01:51 +0500

    login form

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express =  require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res)=>{
+    res.send("this is login form");
+});
+
+app.listen(port, ()=>{
+    console.log(`this app is run on port no. ${port}`);
+});
