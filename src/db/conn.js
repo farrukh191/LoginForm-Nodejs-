71247e5a89665ecf0e9bedbab27c9996c3957ce7
farrukhferoz <farrukhferoz12@gmail.com>
2021-08-02 23:25:07 +0500

    make connection of database

---
+++ src/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/loginform",{
+    useUnifiedTopology:true,
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+}).then(()=>{
+    console.log("connection successfull");
+}).catch((err)=>{
+    console.log(err);
+})
